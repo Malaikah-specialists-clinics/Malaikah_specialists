b734d4c7b299e3537f663b4e5e426a62bbb22082
Winnie-Kiara <winniekk20@gmail.com>
2021-10-16 19:54:11 +0300

    fixed button

--- src/Components/Research.js
+++ src/Components/Research.js
@@ -14,16 +14,18 @@ function Research() {
               owner's food. I am the best try to jump onto window and fall while
               scratching at wall yet while happily ignoring when being called.
               Stare at the wall, play with food and get confused by dust.
+              Meow all night head nudges . Cat not kitten around get scared by
+              sudden appearance of cucumber, bawl under human beds so eat
+              owner's food. I am the best try to jump onto window and fall while
+              scratching at wall yet while happily ignoring when being called.
+              Stare at the wall, play with food and get confused by dust.
             </p>
-            <Button href="#form2" id="Btn" style={{margin:"120px 0px 20px 120px"}} >
+            <Button href="#form2" id="Btn" >
                 LEARN MORE
               </Button>{" "}
           </Col>
           <Col md>
               <img src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/01/327372_1100-1100x628.jpg" alt=""
-              // width="600px"
-              // height="300px"
-              // border-radius="50%"
               style={{
                 width:"600px",
                 height:"300px",
