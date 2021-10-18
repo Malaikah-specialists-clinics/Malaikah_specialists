32187f67c56a056c1911727fa7c123da141c5964
Winnie-Kiara <winniekk20@gmail.com>
2021-10-18 14:26:49 +0300

    fix phone size caption

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,19 +5,26 @@ function Landing() {
   return (
     <>
       {/* id="bgvid" poster="/images/emeservice.jpg" */}
-       <div className="video-container">
-       <video src="/videos/video-dmm.mp4" type="video/mp4" autoPlay muted loop />
-       <div class="caption">
-       <h1>Quality Innovative <br/>
-           Healthcare Delivery
-      </h1>
-      <h2>Easing access to affordable <br/>
-healthcare services. <br/>
-We link global doctors to you
-
-</h2>
-    </div>
-       </div>
+      <div className="video-container">
+        <video
+          src="/videos/video-dmm.mp4"
+          type="video/mp4"
+          autoPlay
+          muted
+          loop
+        />
+        <div id="caption">
+          <h1>
+            Quality Innovative <br />
+            Healthcare Delivery
+          </h1>
+          <p className="caption">
+            Easing access to affordable <br />
+            healthcare services. <br />
+            We link global doctors to you
+          </p>
+        </div>
+      </div>
     </>
   );
 }
