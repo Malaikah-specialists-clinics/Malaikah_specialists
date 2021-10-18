bfb076d373e43cc29e5c3071b61ac98e6c1c2f0a
Winnie-Kiara <winniekk20@gmail.com>
2021-10-18 11:25:56 +0300

    adds video

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,12 @@ import About from './About'
 import Events from './Events';  
 import Research from './Research';
 import Specialities from './Specialities';
+import Landing from './Landing';
 function Home(){
     return (
       <>
       <Container className='formcontent'>
+      <Landing/>
       <Specialities/>
     <Events/>
     <Research/>
