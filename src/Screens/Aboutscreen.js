2dbde2ca55719d346bc87fa3ef6ab838c56844ad
Winnie-Kiara <winniekk20@gmail.com>
2021-11-11 14:21:08 +0300

    fix margins

--- src/Screens/Aboutscreen.js
+++ src/Screens/Aboutscreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Accordion} from 'react-bootstrap'
-import { Row, Col} from 'react-bootstrap'
+// import {Accordion} from 'react-bootstrap'
+// import { Row, Col} from 'react-bootstrap'
 
 
 const Aboutscreen = () => {
