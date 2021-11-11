28c59a0480ee15407ae00383289aa731cf149c98
fred kwesiga <fredkwesiga9@gmail.com>
2021-11-11 20:42:33 +0300

    Changes words to showing one word at a time

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,34 +1,59 @@
-import React from "react";
+import React  from "react";
+import {useEffect, useRef} from "react"
 import { Col } from "react-bootstrap";
 import Alerts from './alertComp';
+import { init } from 'ityped';
 
 function Landing() {
+  const textRef = useRef();
+  // const paragrah = useRef();
+
+  useEffect(() => {
+    console.log(textRef);
+    init(textRef.current, {
+      showCursor: false,
+      typeSpeed: 100,
+      backDelay: 5000,
+      disableBackTyping: false,
+      loop: true,
+
+      strings: ['Quality Innovative', 'Healthcare Delivery'],
+    });
+    // console.log(paragrah);
+    // init(paragrah.current, {
+    //   disableBackTyping: false,
+    //   strings: [
+    //     'Easing access to affordable',
+    //     'healthcare services.',
+    //     'We link global doctors to you',
+    //   ],
+    // });
+  }, []);
   return (
     <>
       {/* id="bgvid" poster="/images/emeservice.jpg" */}
-      <Col> 
-      <div className="video-container">
-        
-        {/* <video
+      <Col>
+        <div className="video-container">
+          {/* <video
           src="/videos/video-dmm.mp4"
           type="video/mp4"
           autoPlay
           muted
           loop
         /> */}
-        <div id="caption" className="video-container">
-        <Alerts />
-          <h1 style={{marginTop:'20%'}}>
-            Quality Innovative <br />
-            Healthcare Delivery
-          </h1>
-          {/* <p className="caption">
-            Easing access to affordable <br />
-            healthcare services. <br />
-            We link global doctors to you
-          </p> */}
+          <div id="caption" className="video-container">
+            <Alerts />
+            <h1 style={{ marginTop: '20%' }}>
+              <span ref={textRef}></span>
+            </h1>
+            <p id="paragrah ">
+              {/* Easing access to affordable <br />
+              healthcare services. <br />
+              We link global doctors to you */}
+              {/* <span ref={paragrah}></span> */}
+            </p>
+          </div>
         </div>
-      </div>
       </Col>
     </>
   );
