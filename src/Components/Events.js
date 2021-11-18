cc9363a872a89830e02e176800331d4ef4466db2
Winnie-Kiara <winniekk20@gmail.com>
2021-11-18 15:23:14 +0300

    changed events screen

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,8 +1,6 @@
 
 import React from "react";
-import { Card, CardGroup, Carousel} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import { Card, CardGroup, Carousel, Col, Row} from "react-bootstrap";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -79,47 +77,39 @@ const Events = () => {
             <h4>Find more Events</h4>
           </a>
         </div>
-        <div class="row">
-          <div class="col-sm-2">
-            <div class="card" id="card1">
-              <div className="item-date"></div>
-              <div>
-                <h3 className="day"> 30</h3>
-                <h3 className="month">NOV</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-8">
-            <div class="card" id="card2">
-              <h2>Food and Nutrition</h2>
+      <Card  style={{height: 'auto', position:'relative', maxWidth:'100%', margin:'auto'}}>
+        <Row style={{height: 'auto'}}>
+           <Col sm={2} style={{paddingTop:'4%', backgroundColor:'#020e20', textAlign: 'center'}}>
+           <h3> 30</h3>
+            <h3>NOV</h3>
+           </Col> 
+           <Col sm={8} style={{border: 'none'}}>
+            <h2>Food and Nutrition</h2>
+           
               <h6>2.00pm-4.00pm</h6>
               <p>
                 The workshop aims to discuss: Preventing noncommunicable
                 dieseases and how to boost immunity.
               </p>
               <div>
-                <h5 style={{ float: 'left' }}>Location:</h5>
-                <h6 style={{ float: 'left', padding: '0.5% 1.5%' }}>Virtual</h6>
+                <h5 style={{ textAlign: 'left' }}>Location: <span style={{fontWeight: 'lighter'}}>Virtual</span> </h5>
               </div>
               <div>
-                <h5 style={{ float: 'left' }}>Contact:</h5>
-                <h6 style={{ float: 'left', padding: '0.5% 1.5%' }}>
-                  malaikah@gmail.com
-                </h6>
-              </div>
-            </div>
-          </div>
-          <div class="pic col-sm-2">
-            <div class="card">
-              <img
-                className="card-image"
+                <h5 style={{ textAlign: 'left' }}>Contact: <span style={{fontWeight: 'lighter'}}> malaikah@gmail.com</span></h5>
+                </div>
+           </Col> 
+           <Col sm={2}>
+           <img
+           className="pic"
                 src="https://images.unsplash.com/photo-1535229398509-70179087ac75?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhbHRoeSUyMGZvb2R8ZW58MHwxfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
                 alt=""
+                style={{height: '200px', width:'100%'}}
               />
-            </div>
-          </div>
-        </div>
+           </Col> 
+           </Row>
+          </Card>
       </div>
+      <div>
       {tip.map((tip_entity) => (
         <CardGroup key={tip_entity.id} style={{width:'94%', margin:'auto'}}>
           <Card id="crd">
@@ -149,6 +139,7 @@ const Events = () => {
           
         </CardGroup>
       ))}
+      </div>
     </div>
   );
 };
