f68ff7c736e3f2724bb3a894fd885c7dcff2402d
fred kwesiga <fredkwesiga9@gmail.com>
2021-11-24 16:53:02 +0300

    adds post method on formscreen

--- src/Screens/FormScreen.js
+++ src/Screens/FormScreen.js
@@ -2,93 +2,191 @@ import React, {Component} from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-
+import axios from "axios";
+// import CORS from "react"
+// import { useState, useEffect } from "react";
 
  
 
 class FormPage extends Component {
-
   alertText = () => {
     alert('You have an appointment!!, Please check your email');
   };
-  render(){
-  return (
-    <>
-      <div
-        className="formcontent"
-        style={{ width: '90%', marginLeft: 'auto', marginRight: 'auto'}}
-      >
-        <Row>
-          <Col md sm={6} className="form">
-            <Form action="http://localhost:3000/bookappts" Method="POST">
-              <h2>Book An Appointment</h2>
-              <h4>Patient Information </h4>
-              <Row>
-                <Col md>
-                  <Form.Label id="field">Email</Form.Label>
-                  <Form.Control
-                    placeholder="winniek@example.com"
-                    id="form-control"
-                    name="email"
-                  />
-                </Col>
-              </Row>
-              <Col md className="">
-                <Row>
-                  <Col md className="inputField">
-                    <Form.Label id="field">Department</Form.Label>
-                    <Form.Select name="sdept" id="form-control" >
-                      <option>Select department</option>
-                      <option >Gynecology</option>
-                      <option >Paediatrics</option>
-                      <option>Non-communicable diseases</option>
-                      <option >Hernia and appendicitis Surgery</option>
-                    </Form.Select>
-                  </Col>
-                  <Col md>
-                    <Form.Label id="field">Date Of Appointment</Form.Label>
-                    <Form.Control name="doa" id="form-control" />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md>
-                    <Form.Label id="field">Time Of Appointment</Form.Label>
-                    <Form.Control name="toa" id="form-control" />
-                  </Col>
-                  <Col md>
-                    <Form.Label id="field">Mode of appointment</Form.Label>
-                    <Form.Select name="moa" id="form-control" >
-                      <option>Select mode of appointment</option>
-                      <option >Online</option>
-                      <option >In Person</option>
-                    </Form.Select>
-                  </Col>
-                </Row>
+  
+  constructor(props) {
+    super(props);
+    
 
-                <Form.Label id="field">
-                  What is your primary medical issue?
-                </Form.Label>
-                <Form.Control name="pmi" type="text-area" id="textarea" />
+    this.state = {
+      email: '',
+      sdpet: '',
+      doa: '',
+      toa: '',
+      moa: '',
+      pmi: '',
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  onChangeSdpet(e) {
+    // this.setState({ sdept: e.target.value });
+    console.log("sdept")
+  }
+  onChangeDoa(e) {
+    this.setState({ doa: e.target.value });
+  }
+
+  onChangeToa(e) {
+    this.setState({ toa: e.target.value });
+  }
+
+  onChangeMoa(e) {
+    this.setState({ moa: e.target.value });
+  }
+
+  onChangePmi(e) {
+    this.setState({ pmi: e.target.value });
+  }
+
+  onSubmit(e) {
+    this.alertText()
+    e.preventDefault();
+ 
+
+    const patientObject = {
+      email: this.state.email,
+      sdept: this.state.sdept,
+      doa: this.state.doa,
+      toa: this.state.toa,
+      moa: this.state.moa,
+      pmi: this.state.pmi,
+    };
+    axios
+      .post('http://localhost:3000/bookappts', patientObject, {headers:{"Access-Control-Allow-Origin":"*"}})
+
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+ 
+
+  render() {
+    return (
+      <>
+        <div
+          className="formcontent"
+          style={{ width: '90%', marginLeft: 'auto', marginRight: 'auto' }}
+        >
+
+          <Row>
+            <Col md sm={6} className="form">
+              <Form onSubmit={(e) => this.onSubmit(e)}>
+                <h2>Book An Appointment</h2>
+                <h4>Patient Information </h4>
                 <Row>
-                  <Col>
-                    {' '}
-                    <Button type="submit" id="Btn" onClick={this.alertText}>
-                      BOOK APPOINTMENT
-                    </Button>{' '}
+                  <Col md>
+                    <Form.Label id="field">Email</Form.Label>
+                    <Form.Control
+                      placeholder="winniek@example.com"
+                      id="form-control"
+                      name="email"
+                      value={this.state.email}
+                      onChange={(e) => this.onChangeEmail(e)}
+                    />
                   </Col>
                 </Row>
-              </Col>
-            </Form>
-          </Col>
-          <Col md>
-            <div className="pic">
-            <img src="/images/caremeds.jpg" alt="doctor" style={{width:'600px', height:'600px', marginTop: '3%'}}/>
-            </div>
-           </Col>
-        </Row>
-      </div>
-    </>
-  );
+                <Col md className="">
+                  <Row>
+                    <Col md className="inputField">
+                      <Form.Label id="field">Department</Form.Label>
+                      <Form.Select
+                        name="sdept"
+                        value={this.state.sdept}
+                        // onChange={(e)=>this.onChangeSdept(e)}
+                        id="form-control"
+                      >
+                        <option>Select department</option>
+                        <option>Gynecology</option>
+                        <option>Paediatrics</option>
+                        <option>Non-communicable diseases</option>
+                        <option>Hernia and appendicitis Surgery</option>
+                      </Form.Select>
+                    </Col>
+                    <Col md>
+                      <Form.Label id="field">Date Of Appointment</Form.Label>
+                      <Form.Control
+                        name="doa"
+                        value={this.state.doa}
+                        onChange={(e) => this.onChangeDoa(e)}
+                        id="form-control"
+                      />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md>
+                      <Form.Label id="field">Time Of Appointment</Form.Label>
+                      <Form.Control
+                        name="toa"
+                        value={this.state.toa}
+                        onChange={(e) => this.onChangeToa(e)}
+                        id="form-control"
+                      />
+                    </Col>
+                    <Col md>
+                      <Form.Label id="field">Mode of appointment</Form.Label>
+                      <Form.Select
+                        name="moa"
+                        value={this.state.moa}
+                        onChange={(e) => this.onChangeMoa(e)}
+                        id="form-control"
+                      >
+                        <option>Select mode of appointment</option>
+                        <option>Online</option>
+                        <option>In Person</option>
+                      </Form.Select>
+                    </Col>
+                  </Row>
+
+                  <Form.Label id="field">
+                    What is your primary medical issue?
+                  </Form.Label>
+                  <Form.Control
+                    name="pmi"
+                    value={this.state.pmi}
+                    onChange={(e) => this.onChangePmi(e)}
+                    type="text-area"
+                    id="textarea"
+                  />
+                  <Row>
+                    <Col>
+                      {' '}
+                      <Button type="submit" id="Btn">
+                        BOOK APPOINTMENT
+                      </Button>{' '}
+                    </Col>
+                  </Row>
+                </Col>
+              </Form>
+            </Col>
+            <Col md>
+              <div className="pic">
+                <img
+                  src="/images/caremeds.jpg"
+                  alt="doctor"
+                  style={{ width: '600px', height: '600px', marginTop: '3%' }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
   }
 }
 export default FormPage;
--- src/Screens/Moeventscreen.js
+++ src/Screens/Moeventscreen.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Col, Row, Alert, Card, Container } from "react-bootstrap";
-import { URL } from '../Constants/index.js';
+import { base_url } from '../Constants/index.js';
 
 const Moeventscreen = () => {
   const [event, setEvents] = useState([]);
   useEffect(() => {
     axios
-      .get(`${URL}/events`)
+      .get(`${base_url}/events`)
       .then((res) => {
         setEvents(res.data);
         console.log(res.data);
@@ -22,41 +22,52 @@ const Moeventscreen = () => {
     return (
       <>
         <div class="hcare">
-          <Container style={{maxWidth: '100%'}}>
-          {event.map((events) => (
-            <Card id='contentcard' style={{marginLeft:'auto', marginRight: 'auto'}}>
-              <Row
-                style={{height: 'auto' }}
-                key={events.id}
-              >
-                <Col sm={2} style={{paddingTop:'4%', backgroundColor:'#020e20', textAlign: 'center'}}>
-                    <h3 className="day">{events.date}</h3>
-                </Col>
-                <Col sm={10} style={{padding: '5px 30px'}}>
-                  <h2>{events.title}</h2>
-                  <h6>{events.time}</h6>
-                  <p>{events.description}</p>
 
-                  <div>
-                    <h5 style={{ textAlign: "left" }}>
-                      Location:{" "}
-                      <span style={{ fontWeight: "lighter" }}> {events.location}</span>{" "}
-                    </h5>
-                  </div>
-                  <div>
-                    <h5 style={{ textAlign: "left" }}>
-                      Contact:{" "}
-                      <span style={{ fontWeight: "lighter" }}>
-                        {" "}
-                        malaikah@gmail.com
-                      </span>
-                    </h5>
-                  </div>
-                  <button className="Btn">REGISTER</button>
-                </Col>
-              </Row>
-            </Card>
-          ))}
+          <Container style={{ maxWidth: '100%' }}>
+            {event.map((events) => (
+              <Card
+                id="contentcard"
+                style={{ marginLeft: 'auto', marginRight: 'auto' }}
+              >
+                <Row style={{ height: 'auto' }} key={events.id}>
+                  <Col
+                    sm={2}
+                    style={{
+                      paddingTop: '4%',
+                      backgroundColor: '#020e20',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <h3 className="day">{events.date}</h3>
+                  </Col>
+                  <Col sm={10} style={{ padding: '5px 30px' }}>
+                    <h2>{events.title}</h2>
+                    <h6>{events.time}</h6>
+                    <p>{events.description}</p>
+
+                    <div>
+                      <h5 style={{ textAlign: 'left' }}>
+                        Location:{' '}
+                        <span style={{ fontWeight: 'lighter' }}>
+                          {' '}
+                          {events.location}
+                        </span>{' '}
+                      </h5>
+                    </div>
+                    <div>
+                      <h5 style={{ textAlign: 'left' }}>
+                        Contact:{' '}
+                        <span style={{ fontWeight: 'lighter' }}>
+                          {' '}
+                          malaikah@gmail.com
+                        </span>
+                      </h5>
+                    </div>
+                    <button className="Btn">REGISTER</button>
+                  </Col>
+                </Row>
+              </Card>
+            ))}
           </Container>
         </div>
       </>
