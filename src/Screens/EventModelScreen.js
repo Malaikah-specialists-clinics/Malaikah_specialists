9c02f90efb63739bbc5487be63ea5cff9153e9df
Winnie-Kiara <winniekk20@gmail.com>
2021-12-16 12:51:24 +0300

    adds whatsapp icon

--- src/Screens/EventModelScreen.js
+++ src/Screens/EventModelScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 import { base_url } from '../Constants/index.js';
