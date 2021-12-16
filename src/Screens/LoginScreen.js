594b82c4016975a36ad593c082b8ae023ff5ac17
fred kwesiga <fredkwesiga9@gmail.com>
2021-12-16 11:17:41 +0300

    validates the register form for events

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,55 +1,72 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { Row, Col, Form, Button} from "react-bootstrap";
 import axios from 'axios';
 import { base_url } from '../Constants/index.js';
-// import '../validation';
+import { Formik} from 'formik';
 
-
-
-class Login extends Component {
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     email: '',
-  //     password: '',
-  //   };
-  // }
-  changeHandler = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-   onSubmit(e) {
-    e.preventDefault();
-  
-    const loginObject = {
-      email: this.state.email,
-      password: this.state.password,
-    };
-    axios
-      .post(`${base_url}/users/login`, loginObject, {
-        headers: { 'Access-Control-Allow-Origin': '*' },
-      })
-
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      // this.props.history.push('/profile');
-      
+const validateLogin = (userLogin) => {
+  const errors = {};
+  if (!userLogin.email) {
+    errors.email = 'Please Enter Email';
+  } else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(userLogin.email)
+  ) {
+    errors.email = 'Invalid email address';
   }
+  if (!userLogin.password) {
+    errors.password = 'Please enter correct password';
+  } else {
+    errors.password = 'correct password'
+  }
+
+  return errors;
+};
+
+
+
+
+const Login =()=> {
+
+  // changeHandler = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+
+  //  onSubmit(e) {
+  //   e.preventDefault();
+  
+  //   const loginObject = {
+  //     email: this.state.email,
+  //     password: this.state.password,
+  //   };
+  //   axios
+  //     .post(`${base_url}/users/login`, loginObject, {
+  //       headers: { 'Access-Control-Allow-Origin': '*' },
+  //     })
+
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  //     // this.props.history.push('/profile');
+      
+  // }
  
-  render(){
     return (
       <>
         <div
           className="formcontent"
-          style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto', marginTop: '8%' }}
+          style={{
+            width: '80%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginTop: '8%',
+          }}
         >
           <Row>
             <Col md>
-              <h2 style={{textAlign: 'center' }}>Login</h2>
+              <h2 style={{ textAlign: 'center' }}>Login</h2>
               <div id="pic">
                 <img
                   src="/images/doc.jpg"
@@ -60,61 +77,74 @@ class Login extends Component {
               </div>
             </Col>
             <Col md sm={6} className="form">
-              <Form
-                onSubmit={(e) => this.onSubmit(e)}
-                
-                style={{ padding: '10px' }}
+              <Formik
+                initialValues={{
+                  email: '',
+                  password: '',
+                }}
+                validate={validateLogin}
+                onSubmit={(values) => {
+                  const loginObject = {
+                    email: values.email,
+                    password: values.password,
+                  };
+
+                  axios.post(`${base_url}/event-reg`, loginObject, {
+                    headers: { 'Access-Control-Allow-Origin': '*' },
+                  });
+                }}
               >
-                <Row>
-                  <Col md>
-                    <Form.Label id="field">Email Address</Form.Label>
-                    <Form.Control
-                      name="email"
-                      value={this.state.email}
-                      onChange={this.changeHandler}
-                      // onChangeText={handleChange('email')}
-                      id="email"
-                    />
-                    <span id="EmailError"></span>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md>
-                    <Form.Label id="field">Password</Form.Label>
-                    <Form.Control
-                      name="password"
-                      value={this.state.password}
-                      onChange={this.changeHandler}
-                      type="password"
-                      id="form-control"
-                    />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col style={{ marginTop: '5%' }}>
-                    <Button type="submit"
-                     id="Btn" 
-                     onSubmit="user()"
-                     >
-                      LOGIN
-                    </Button>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col style={{ marginTop: '9.5%' }}>
-                    <p>
-                      Have No account? <a href="#register">Sign Up</a>
-                    </p>
-                  </Col>
-                </Row>
-              </Form>
+                {(formik) => (
+                  <Form
+                    onSubmit={(e) => this.onSubmit(e)}
+                    style={{ padding: '10px' }}
+                  >
+                    <Row>
+                      <Col md>
+                        <Form.Label id="field">Email Address</Form.Label>
+                        <Form.Control
+                          name="email"
+                          value={formik.values.email}
+                          onChange={formik.handleChange}
+                          id="email"
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md>
+                        <Form.Label id="field">Password</Form.Label>
+                        <Form.Control
+                          name="password"
+                          value={formik.values.password}
+                          onChange={formik.handleChange}
+                          type="password"
+                          id="form-control"
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col style={{ marginTop: '5%' }}>
+                        <Button type="submit" id="Btn" onSubmit="user()">
+                          LOGIN
+                        </Button>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col style={{ marginTop: '9.5%' }}>
+                        <p>
+                          Have No account? <a href="#register">Sign Up</a>
+                        </p>
+                      </Col>
+                    </Row>
+                  </Form>
+                )}
+              </Formik>
             </Col>
           </Row>
         </div>
       </>
     );
-  }
-  }
+}
   
 
 export default Login;
