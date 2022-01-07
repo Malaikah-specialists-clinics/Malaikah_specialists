9d41edc508e53892d5adbeacff3f83037a998082
Winnie-Kiara <winniekk20@gmail.com>
2022-01-07 14:06:38 +0300

    fix icon

--- src/EducationPortal/StudentLogin.js
+++ src/EducationPortal/StudentLogin.js
@@ -1,33 +1,36 @@
 import React from "react";
-import { Container, Form, Button, Row, Col} from "react-bootstrap";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
 
 const StudentLogin = () => {
   return (
     <>
-    <Container style={{marginTop:'15%', marginBottom: '5%'}}>
-      <Form className="stdform">
-      <Row>
-        <Col xs lg="6"> <h2>LOGIN</h2></Col>
-        <Col xs lg="3"><h4>Student</h4></Col>
-        <Col xs lg="3"> <h4>Facilitator</h4></Col>
-      </Row>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Email" />
-        </Form.Group>
+      <Container style={{ marginTop: "15%", marginBottom: "5%" }}>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <div style={{textAlign:'center'}}>
-          
-        <Button id="Btn" type="submit" href="#studentdashboard">  SUBMIT </Button>
-        
-        </div>
-        
-         
-      
-      </Form>
-    </Container>
+        <Form className="stdform">
+          <Row>
+            <Col xs lg="6">
+              <h2>LOGIN</h2>
+            </Col>
+            <Col xs lg="3">
+              <h4>Student</h4>
+            </Col>
+            <Col xs lg="3">
+              <h4>Facilitator</h4>
+            </Col>
+          </Row>
+
+          <Form.Control type="email" placeholder="Email" id='field' />
+
+          <Form.Control type="password" placeholder="Password" id='field' />
+
+          <div style={{ textAlign: "center" }}>
+            <Button id="Btn" type="submit" href="#studentdashboard">
+              {" "}
+              SUBMIT{" "}
+            </Button>
+          </div>
+        </Form>
+      </Container>
     </>
   );
 };
