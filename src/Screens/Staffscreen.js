938c1cc785a562d9a75ac5eb40199ce58e74d65d
fred kwesiga <fredkwesiga9@gmail.com>
2022-01-21 16:36:10 +0300

    works on .mapping for specialties

--- src/Screens/Staffscreen.js
+++ src/Screens/Staffscreen.js
@@ -6,7 +6,6 @@ const Staffscreen = (props) => {
     return (
       <>
       <Container style={{maxWidth: '100%'}}>
-        <div class="hcare">
         <h2 style={{ textAlign: 'center', marginTop: '10px'}}> Meet Our Doctors</h2>
             <CardGroup style={{justifyContent: 'space-evenly'}}>
               <StaffComp  Img="/images/doc.jpg" title="DR. COLLINES K.B" body="Experience: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece  of classical Latin literature from 45 BC,  making "/>
@@ -18,7 +17,6 @@ const Staffscreen = (props) => {
               <StaffComp Img="/images/doc.jpg" title="DR. BAKER AGABA" body="Experience: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece  of classical Latin literature from 45 BC,  making "/>
               <StaffComp Img="/images/doc.jpg" title="DR. ALICIA KWESIGA" body="Experience: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece  of classical Latin literature from 45 BC,  making "/>
             </CardGroup>
-        </div>
         </Container>
       </>
     );
