fa0f872c91f7384d16f307db0a2e3c899d4cb9b1
Winnie-Kiara <winniekk20@gmail.com>
2022-01-27 00:07:15 +0300

    adds research comments modal

--- src/Screens/ResearchScreen.js
+++ src/Screens/ResearchScreen.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
-import { Col, Row, Container } from "react-bootstrap";
+import React, {useState}from "react";
+import { Col, Row, Container, Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-class Research extends Component {
-  render() {
+const Research = () => {
+  
+  
+ const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return (
       <>
         <div className="res-bimg">
@@ -45,28 +50,24 @@ class Research extends Component {
               <p>A research about COVID 19 vaccine in Africa ......</p>
               <Row>
                 <Col style={{ marginTop: "5px" }}>
-                  <Link
-                    class="btn btn-primary"
-                    to=""
-                    role="button"
-                    style={{ background: "#0A1F3E", border: 'none' }}
-                  >
+                  
                     25/04/2021
-                  </Link>
+                 
                 </Col>
                 <Col md style={{ marginTop: "5px" }}>
-                  <Link
-                    class="btn btn-primary"
-                    to=""
-                    role="button"
-                    style={{
-                      background: "white",
-                      color: "black",
-                      border: "none",
-                    }}
-                  >
-                    25 comments
-                  </Link>
+                  <Button style={{background:'#0a1f3e', border: 'none'}} onClick={handleShow}>
+      Comments
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <input />
+        </Modal.Footer>
+      </Modal>
                 </Col>
                 <Col md style={{ marginTop: "5px" }}>
                   <Link
@@ -83,69 +84,11 @@ class Research extends Component {
           </div>
         </div>
 
-        <div
-          class="row"
-          style={{ height: "auto", marginTop: "30px" }}
-        >
-          <div class="col-sm-4">
-            <div class="card-box" id="card4">
-              <div class="card-thumbnail">
-                <img
-                  className="card-image"
-                  src="https://media.msf.org/AssetLink/407080s4p5src3vqpf1mra5oq7t4t8w5.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-8">
-            <div id="card5">
-              <h2>A new discovery</h2>
-
-              <p>Recent publication on psycholigical disorder ......</p>
-              <Row>
-                <Col md style={{ marginTop: "5px" }}>
-                  <Link
-                    class="btn btn-primary"
-                    to=""
-                    role="button"
-                    style={{ background: "#0A1F3E", border: 'none' }}
-                  >
-                    25/04/2021
-                  </Link>
-                </Col>
-                <Col md style={{ marginTop: "5px" }}>
-                  <Link
-                    class="btn btn-primary"
-                    to=""
-                    role="button"
-                    style={{
-                      background: "white",
-                      color: "black",
-                      border: "none",
-                    }}
-                  >
-                    25 comments
-                  </Link>
-                </Col>
-                <Col md style={{ marginTop: "5px" }}>
-                  <Link
-                    class="btn btn-primary"
-                    to=""
-                    role="button"
-                    style={{ background: "#FBBB35", color: "black", border: 'none' }}
-                  >
-                    Read More
-                  </Link>
-                </Col>
-              </Row>
-            </div>
-          </div>
-        </div>
+        
         </Container>
         </>
     );
-  }
+ 
 }
 
 export default Research;
